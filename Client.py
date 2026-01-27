49254566a067acb4ca32b03c8b595ff5c2ca65e4
F7P-H4NN1B4L <101667659+F7P-H4NN1B4L@users.noreply.github.com>
2026-01-27 15:36:33 +0100

    Update Client.py

--- Client.py
+++ Client.py
@@ -7,8 +7,8 @@ import platform
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_ip = '192.168.1.70'
-server_port = 8080
+server_ip = '0.0.0.0'
+server_port = 4444
 
 server_address = (server_ip, server_port)
 
